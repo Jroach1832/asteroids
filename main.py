a9d85bf309e132ad89ff7f3e41a2b2c885373931
jroach1832 <roach.jonathon4@gmail.com>
2024-08-30 06:00:55 -0500

    adding group management

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
-    
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -17,8 +20,14 @@ def main():
         dt = clock.tick(60) / 1000  
         
         screen.fill((0, 0, 0))  # Fill the screen with black before drawing the player
-        player.draw(screen)
-        player.update(dt)
+        
+        #player.draw(screen)
+        for dsprite in drawable:
+            dsprite.draw(screen)
+
+        #player.update(dt)
+        for usprite in updatable:
+            usprite.update(dt)
             
         pygame.display.flip()  # Update the display
 
